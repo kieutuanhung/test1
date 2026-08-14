c22be178b2ba16ea0fa35eb93d444afc32a9519b
lowkeystudy <idkwhyiamusingthisshjt@gmail.com>
2026-08-14 22:22:04 +0700

    Them tai khoan Admin mac dinh vao Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Tạo tài khoản Admin mặc định nếu chưa tồn tại
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name'     => 'Quản Trị Viên',
+                'password' => Hash::make('12345678'), // Mật khẩu đăng nhập
+                'role'     => 'admin',
+            ]
+        );
     }
 }
